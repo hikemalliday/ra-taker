9bf4afd53c2af6b85c7f0889e3b75db2871d0dfb
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2025-11-18 21:07:39 -0600

    added POST to website from command "take_ra_and_post"

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ async def take_ra_and_post(ctx):
                 REST_URI,
                 data={"players_list": rows},
                 headers={
+                    "Api-Key": API_KEY,
                 }
             )
             if response.status_code in (200, 201):
