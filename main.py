7251da191b4975821c97d18b75a387b43b1b136d
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2026-02-15 11:18:03 -0600

    removed 'verify' kwarg from take_ra command

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def take_ra(interaction: discord.Interaction, raid_name: str):
                 REST_URI,
                 json=payload,
                 headers={"Authorization": f"Api-Key {API_KEY}"},
-                verify=False  # Disable SSL verification for expired cert
+                # verify=False  # Disable SSL verification for expired cert
             )
 
             if response.status_code in (200, 201):
